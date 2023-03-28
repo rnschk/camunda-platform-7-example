b99b838bf7f9bd4439f32068d6934c4552046ccd
rnschk <5700718+rnschk@users.noreply.github.com>
2023-03-28 12:00:11 +0200

    add parent/sub-processes

---
+++ process-solution-1/src/main/java/de/rnschk/cam/process1/Delegates.java
@@ -0,0 +1,24 @@
+package de.rnschk.camunda.process1;
+
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.camunda.spin.plugin.variable.SpinValues.jsonValue;
+
+@Configuration
+public class Delegates {
+    private static final Logger log = LoggerFactory.getLogger(Delegates.class);
+
+    @Bean
+    public JavaDelegate delegate1() {
+        return execution -> log.info("Hello from delegate1!");
+    }
+
+    @Bean
+    public JavaDelegate generateSubprocesses() {
+        return execution -> execution.setVariable("subprocesses", jsonValue("[\"subprocess1\", \"subprocess2\"]"));
+    }
+}
