1c729931f54d0a07d0648cd823c92cfc51c9258b
rnschk <5700718+rnschk@users.noreply.github.com>
2023-03-28 11:59:34 +0200

    restructure

---
+++ external-task-handler/src/main/java/de/rnschk/cam/ExternalHandlerApp.java
@@ -0,0 +1,13 @@
+package de.rnschk.camunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalHandlerApp {
+
+    public static void main(String... args) {
+        SpringApplication.run(ExternalHandlerApp.class, args);
+    }
+
+}
