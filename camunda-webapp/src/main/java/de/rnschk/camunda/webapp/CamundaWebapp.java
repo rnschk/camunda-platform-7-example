7b9d718c0a20a92068f91f63e1ddde0b587a36d5
rnschk <5700718+rnschk@users.noreply.github.com>
2023-03-29 15:52:04 +0200

    add custom branding to webapp

--- camunda-webapp/src/main/java/de/rnschk/camunda/webapp/CamundaWebapp.java
+++ camunda-webapp/src/main/java/de/rnschk/camunda/webapp/CamundaWebapp.java
@@ -1,12 +1,17 @@
 package de.rnschk.camunda.webapp;
 
+import de.rnschk.camunda.webapp.branding.CustomBrandingProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({CustomBrandingProperties.class})
 public class CamundaWebapp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CamundaWebapp.class, args);
-	}
+  // Custom Branding: https://github.com/viadee/camunda-platform-7-examples/tree/main/cockpit/custom-branding
+
+  public static void main(String[] args) {
+    SpringApplication.run(CamundaWebapp.class, args);
+  }
 }
