2cc6e77bbe8052aba8d85cb1e0bfc30eba410124
rnschk <5700718+rnschk@users.noreply.github.com>
2024-05-24 17:15:56 +0200

    add schema-service

---
+++ camunda-db-schema-service/src/main/java/de/rnschk/camunda/db/CamundaDbSchemaService.java
@@ -0,0 +1,12 @@
+package de.rnschk.camunda.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaDbSchemaService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaDbSchemaService.class, args);
+	}
+}
