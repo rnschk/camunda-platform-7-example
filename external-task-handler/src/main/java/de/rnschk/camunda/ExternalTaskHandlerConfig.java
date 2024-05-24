cdd09e2db2a0f97df709ac9ed57a13a76b4928be
rnschk <5700718+rnschk@users.noreply.github.com>
2024-05-24 17:18:03 +0200

    better process

--- external-task-handler/src/main/java/de/rnschk/camunda/ExternalTaskHandlerConfig.java
+++ external-task-handler/src/main/java/de/rnschk/camunda/ExternalTaskHandlerConfig.java
@@ -1,16 +1,30 @@
 package de.rnschk.camunda;
 
+import jakarta.annotation.PostConstruct;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.client.backoff.BackoffStrategy;
 import org.camunda.bpm.client.backoff.ExponentialBackoffStrategy;
 import org.camunda.bpm.client.spring.annotation.ExternalTaskSubscription;
 import org.camunda.bpm.client.task.ExternalTaskHandler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+import java.util.Map;
 
 @Slf4j
 @Configuration
 public class ExternalTaskHandlerConfig {
+
+    @Value("${camunda.bpm.client.worker-id}")
+    private String workerId;
+
+    @PostConstruct
+    public void hello() {
+        log.info("Hallo worker '{}'", shortMeIfYouCan(workerId));
+    }
 
     @Bean
     public BackoffStrategy backoffStrategy() {
@@ -21,8 +35,10 @@ public class ExternalTaskHandlerConfig {
     @ExternalTaskSubscription("topic1")
     public ExternalTaskHandler topic1() {
         return (task, service) -> {
-            log.info("doing some work for topic1");
-            service.complete(task);
+            sleep();
+            var who = shortMeIfYouCan(workerId);
+            log.info("worker '{}' doing some work for topic1", who);
+            service.complete(task, Map.of("workerId", who));
         };
     }
 
@@ -30,9 +46,20 @@ public class ExternalTaskHandlerConfig {
     @ExternalTaskSubscription("topic2")
     public ExternalTaskHandler topic2() {
         return (task, service) -> {
-            log.info("doing some work for topic2");
-            service.complete(task);
+            sleep();
+            var who = shortMeIfYouCan(workerId);
+            log.info("worker '{}' doing some work for topic2", who);
+            service.complete(task, Map.of("workerId", who));
         };
+    }
+
+    @SneakyThrows
+    private static void sleep() {
+        Thread.sleep(Duration.ofSeconds(2));
+    }
+
+    private static String shortMeIfYouCan(final String uuid) {
+        return uuid.split("-")[0];
     }
 
 }
